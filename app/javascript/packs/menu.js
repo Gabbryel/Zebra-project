cf20fd386d20695dfcf10252ee8242275a1d78be
Gabriel Ursache <gabriel@markov.ro>
2020-08-16 21:22:54 +0300

    small mennu on top

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -2,7 +2,8 @@ function menu () {
     const menu = document.getElementById('menu');
     const burgerMenu = document.getElementById('burger-menu');
     const x = document.getElementById('close-menu');
-    
+    const navBurgerMenu = document.getElementById('nav-burger--menu');
+
     const openMenu = () => {
       burgerMenu.addEventListener('click', () => {
         if (menu.style.display === 'none') {
@@ -10,8 +11,13 @@ function menu () {
         } else {
           menu.style.display = 'none';
         }
-         
       })  
+    }
+
+    const openMenuFromNavbar = () => {
+        navBurgerMenu.addEventListener('click', () => {
+            menu.style.display = 'inline';
+        })
     }
     
     const closeMenu = () => {
@@ -20,9 +26,10 @@ function menu () {
       })
     }
 
-    if ( x && burgerMenu) {
+    if ( x && burgerMenu && menu) {
         openMenu();
         closeMenu();
+        openMenuFromNavbar();
     }
 }
 
